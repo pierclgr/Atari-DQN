2fac89722b15d6e025e3cdeb523e7d0f3417e4c9
Pierpasquale Colagrande <pierpasquale.colagrande@univrses.com>
2022-08-11 16:21:22 +0200

    Add requirements

--- src/shell_scripts/colab_prepare.sh
+++ src/shell_scripts/colab_prepare.sh
@@ -1,27 +1,23 @@
-# install x11-utils
 echo "Installing packages for colab..."
-apt-get install x11-utils
 
-# install pyglet
-pip install pyglet
+# install repo package
+cd /content/Atari-Deep-RL
+pip install -e .
+pip install -r requirements.txt
+cd /content/
+
+# install x11-utils
+apt-get install x11-utils
 
 # install python opengl
 apt-get install -y xvfb python-opengl ffmpeg
-
-# install pyvirtualdisplay
-pip install pyvirtualdisplay
-
-# install colabgymrender to render the agent on colab
-pip install -U colabgymrender
-
-# install gym environments
-pip install gym[box2d]
-pip install gym[atari]
 
 # download atari roms from the web and copy them to gym folder
 wget http://www.atarimania.com/roms/Roms.rar
 unrar x -Y "Roms.rar" "roms/"
 python -m atari_py.import_roms "roms/"
 
+# remove roms
 rm -r "roms/"
 rm "Roms.rar"
+

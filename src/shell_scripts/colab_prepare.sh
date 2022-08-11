e83798b0d81bac8ba96ea782b80bd97fd6f292c8
Pierpasquale Colagrande <pierpasquale.colagrande@univrses.com>
2022-08-11 18:00:50 +0200

    Fix

--- src/shell_scripts/colab_prepare.sh
+++ src/shell_scripts/colab_prepare.sh
@@ -3,13 +3,8 @@ echo "Installing packages for colab..."
 # install repo package
 cd /content/Atari-Deep-RL
 pip install -e .
+pip install -r requirements.txt
 cd /content/
-
-pip install imageio-ffmpeg
-pip install pyglet
-pip install pyvirtualdisplay
-pip install -U colabgymrender
-pip install gym[atari]
 
 # install x11-utils
 apt-get install x11-utils
